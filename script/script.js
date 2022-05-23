6a6eebd5194cb26045d7f379696daa859da20cc6
jean paul <jeanpaulabizeid@gmail.com>
2022-05-23 10:19:44 +0300

    creating an array to choose image from

--- script/script.js
+++ script/script.js
@@ -1,2 +1,5 @@
 var random_number_1 = Math.floor(Math.random() * 6);    //generating numbers between 0 and 5
 var random_number_2 = Math.floor(Math.random() * 6);
+
+//array to select dice image
+const image_source = ["./images/dice1.png", "./images/dice2.png", "./images/dice3.png", "./images/dice4.png", "./images/dice5.png", "./images/dice6.png" ]
