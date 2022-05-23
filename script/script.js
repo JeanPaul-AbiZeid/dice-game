ce5a6965e883078b43345f5af90114bfe4f91c08
jean paul <jeanpaulabizeid@gmail.com>
2022-05-23 10:31:54 +0300

    creating two functions to display the random dice

--- script/script.js
+++ script/script.js
@@ -6,3 +6,17 @@ const image_source = ["./images/dice1.png", "./images/dice2.png", "./images/dice
 
 var player1_image = image_source[random_number_1];
 var player2_image = image_source[random_number_2];
+
+var img1 = document.getElementsByClassName("img1")
+var img2 = document.getElementsByClassName("img2")
+
+function player1() {
+    img1[0].src = player1_image;
+}
+
+function player2() {
+    img2[0].src = player2_image;
+}
+
+player1()
+player2()
